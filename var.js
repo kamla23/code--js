f9131675f77d78ddce157f3a7f838f23072d3792
kamla23 <kamlabai@23navgurukul.org>
2025-01-02 12:22:30 +0530

    first commit

---
+++ var.js
@@ -0,0 +1,14 @@
+let student = {
+    college : "gilrs college",
+    subject : "maths",
+    rollno : 1234,
+}
+student.Resume = 12;
+delete student.subject;
+console.log(student);
+console.log(student.rollno);
+
+// let name = "kamla";
+// console.log(typeof name);
+
+
